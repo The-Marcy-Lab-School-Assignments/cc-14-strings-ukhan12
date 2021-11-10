5e3e5986ba32f366426ffef247468d08479c6b5d
uzma <ubkhan3@gmail.com>
2021-11-10 15:05:42 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+
+function stripUpperCase(str){
+    let newString="";
+    for(let i = 0; i < str.length; i++){
+        if(str[i].toLowerCase() === str[i]){
+            newString += str[i];
+        }
+    }
+    return newString;
+}
